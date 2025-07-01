9e5b8e6ba752f43844cb5bb76949db5570da0fde
Uria Dutu <balisoan03@gmail.com>
2025-07-01 16:36:05 +0800

    data

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ app.include_router(prapemrosesan.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3001",
-        "http://127.0.0.1:3001"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
